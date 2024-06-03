95f00ddb2cd3081a29fcfc21cfd831c52f51e7c3
Vasil Genov <vasil.genov@psinteractive.io>
2024-06-03 12:09:33 +0300

    feat: cleanup

--- data/store.ts
+++ data/store.ts
@@ -21,6 +21,7 @@ export type NodeData = {
 }
 
 export type RFState = {
+  // flow state
   nodes: Node<NodeData>[]
   edges: Edge[]
   onNodesChange: OnNodesChange
@@ -28,39 +29,31 @@ export type RFState = {
   onConnect: OnConnect
   setNodes: (nodes: Node[]) => void
   setEdges: (edges: Edge[]) => void
-  // updateNodeColor: (nodeId: string, color: string) => void
-  updateNodeTitle: (nodeId: string, title: string) => void
+  // custom state
   currentNodeId?: string
+  updateNodeTitle: (nodeId: string, title: string) => void
   setCurrentNodeId: (nodeId: string) => void
 }
 
-// this is our useStore hook that we can use in our components to get parts of the store and call actions
 const useStore = create<RFState>((set, get) => ({
   nodes: initialNodes,
   edges: initialEdges,
   currentNodeId: undefined,
-  onNodesChange: (changes: NodeChange[]) => {
+  onNodesChange: (changes: NodeChange[]) =>
     set({
       nodes: applyNodeChanges(changes, get().nodes),
-    })
-  },
-  onEdgesChange: (changes: EdgeChange[]) => {
+    }),
+  onEdgesChange: (changes: EdgeChange[]) =>
     set({
       edges: applyEdgeChanges(changes, get().edges),
-    })
-  },
-  onConnect: (connection: Connection) => {
+    }),
+  onConnect: (connection: Connection) =>
     set({
       edges: addEdge(connection, get().edges),
-    })
-  },
-  setNodes: (nodes: Node[]) => {
-    set({ nodes })
-  },
-  setEdges: (edges: Edge[]) => {
-    set({ edges })
-  },
-  updateNodeTitle: (nodeId: string, title: string) => {
+    }),
+  setNodes: (nodes: Node[]) => set({ nodes }),
+  setEdges: (edges: Edge[]) => set({ edges }),
+  updateNodeTitle: (nodeId: string, title: string) =>
     set({
       nodes: get().nodes.map(node => {
         if (node.id === nodeId) {
@@ -69,8 +62,7 @@ const useStore = create<RFState>((set, get) => ({
         }
         return node
       }),
-    })
-  },
+    }),
   setCurrentNodeId: (nodeId: string) => set({ currentNodeId: nodeId }),
 }))
 
