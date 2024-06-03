7c36b963abb3aa09d855159b64ad033a1082c2e2
Vasil Genov <vasil.genov@psinteractive.io>
2024-06-03 13:55:47 +0300

    feat: sourceHandle

--- data/edges.ts
+++ data/edges.ts
@@ -2,6 +2,6 @@ import type { Edge } from 'reactflow'
 
 export default [
   { id: 'edge-0', source: 'node-0', target: 'node-1' },
-  { id: 'edge-1', source: 'node-1', target: 'node-2', sourceHandle: 'r1' },
-  { id: 'edge-2', source: 'node-1', target: 'node-3', sourceHandle: 'r2' },
+  { id: 'edge-1', source: 'node-1', sourceHandle: 'r1', target: 'node-2' },
+  { id: 'edge-2', source: 'node-1', sourceHandle: 'r2', target: 'node-3' },
 ] as Edge[]
